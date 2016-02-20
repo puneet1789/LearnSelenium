87bb6bbaf9126e748757f18749f3dbefc02753f2
puneet gupta <puneetgupta1789@gmail.com>
2016-02-21 01:29:20 +0530

    2nd commit

--- src/mypackage/Login.java
+++ src/mypackage/Login.java
@@ -1,10 +1,17 @@
 package mypackage;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Login {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-	}
 
+		FirefoxDriver FF = new FirefoxDriver();
+	
+		FF.get("http://www.fb.com");	
+		Thread.sleep(5000);
+		
+	}
 }
